a38488fd638ea371cad668b152ed6d3828cea6d3
Roopa Thimmanacherla <roopareddy90@gmail.com>
2023-10-27 23:13:44 -0700

    added css

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ var startbtn = document.querySelector("#start-quiz ");
 var timerElement = document.querySelector("#timer-count");
 var answer = document.querySelector(".answer");
 var questionContainer = document.getElementById("questionContainer");
-var timerCount;
+var quizStartPage = document.getElementById("startPage");
 var currentQuestionIndex = 0;
 var btn;
 var score = 0;
-
+var secondsLeft = 50;
 var questions = [
   {
     q: "Inside which HTML do we put the javascript?",
@@ -28,7 +28,7 @@ var questions = [
       "getElementById(id)",
       "elementById(id)",
     ],
-    a: "elementById(id)",
+    a: "getElementById(id)",
   },
 
   {
@@ -57,37 +57,66 @@ var questions = [
 startbtn.addEventListener("click", startQuiz);
 
 function startQuiz() {
+  quizStartPage.innerHTML = "";
   renderQuestions();
   console.log("start quiz function");
+  setTime();
+}
+
+function checkAnswer(event) {
+  console.log(event.target);
+  if (event.target.value === questions[currentQuestionIndex].a) {
+    answer.innerHTML = "correct!";
+  } else {
+    answer.innerHTML = "Wrong!";
+  }
+  if (currentQuestionIndex < questions.length - 1) {
+    currentQuestionIndex++;
+    renderQuestions();
+  } else {
+    endQuiz();
+  }
 }
 
 function renderQuestions() {
-  for (
-    currentQuestionIndex = 0;
-    currentQuestionIndex < questions.length;
-    currentQuestionIndex++
-  ) {
-    var currentQuestion = questions[currentQuestionIndex];
-    var qst = document.createElement("h3");
-    qst.innerText = currentQuestion.q;
-    questionContainer.append(qst);
+  questionContainer.innerHTML = "";
+  var currentQuestion = questions[currentQuestionIndex];
+  var qst = document.createElement("h3");
+  qst.innerText = currentQuestion.q;
+  questionContainer.append(qst);
 
-    for (j = 0; j < currentQuestion.choices.length; j++) {
-      var currentChoice = currentQuestion.choices[j];
-      btn = document.createElement("button");
-      btn.setAttribute("value", currentChoice);
-      btn.textContent = currentChoice;
-      questionContainer.appendChild(btn);
-      btn.addEventListener("click", checkAnswer);
-    }
-    function checkAnswer(event) {
-      console.log(event.target);
-      if (btn.value === questions[currentQuestionIndex].a) {
-        score += 10;
-        answer.innerText = "correct!";
-      } else {
-        answer.innerText = "Wrong!";
-      }
-    }
+  for (j = 0; j < currentQuestion.choices.length; j++) {
+    var currentChoice = currentQuestion.choices[j];
+    btn = document.createElement("button");
+    btn.setAttribute("value", currentChoice);
+    btn.textContent = currentChoice;
+    questionContainer.appendChild(btn);
+    btn.addEventListener("click", checkAnswer);
   }
+}
+
+function setTime() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+
+    timerElement.textContent = "Time: " + secondsLeft;
+
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+}
+function endQuiz() {
+  questionContainer.innerHTML = "";
+
+  var initials = document.getElementById("initialSubmit");
+  var endMessage = document.createElement("h2");
+  endMessage.innerHTML = "ALL DONE";
+  initials.append(endMessage);
+  var elemForm = document.createElement("form");
+  elemForm.appendChild(document.createElement("textarea"));
+  initials.append(elemForm);
+  var submitButton = document.createElement("input");
+  submitButton.setAttribute("type", "submit");
+  initials.append(submitButton);
 }
