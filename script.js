fa66a098cb038f1ad6f7db1620511ed25b36bea4
Roopa Thimmanacherla <roopareddy90@gmail.com>
2023-10-28 19:38:19 -0700

    added code to store and retrieve data from local storage

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ var timerElement = document.querySelector("#timer-count");
 var answer = document.querySelector("#answer");
 var questionContainer = document.getElementById("questionContainer");
 var quizStartPage = document.getElementById("startPage");
+var initialSubmitContainer = document.getElementById("initialSubmit");
+var displayHighScores = document.getElementById("highScoreContainer");
 var currentQuestionIndex = 0;
+var setHighScores = JSON.parse(localStorage.getItem("setHighScores")) || [];
+
 var btn;
 var score = 0;
 var secondsLeft = 50;
@@ -66,22 +70,29 @@ function startQuiz() {
 function checkAnswer(event) {
   console.log(event.target);
   if (event.target.value === questions[currentQuestionIndex].a) {
-    answer.innerHTML = "correct!";
+    answer.innerHTML = "Correct!";
   } else {
     answer.innerHTML = "Wrong!";
+    if (secondsLeft >= 10) {
+      secondsLeft -= 10;
+    }
   }
-  if (currentQuestionIndex < questions.length - 1) {
-    currentQuestionIndex++;
-    renderQuestions();
-  } else {
-    endQuiz();
-  }
-}
 
+  // Add a delay to display the message
+  setTimeout(function () {
+    answer.innerHTML = "";
+    if (currentQuestionIndex < questions.length - 1) {
+      currentQuestionIndex++;
+      renderQuestions();
+    } else {
+      endQuiz();
+    }
+  }, 1000);
+}
 function renderQuestions() {
   questionContainer.innerHTML = "";
   var currentQuestion = questions[currentQuestionIndex];
-  var qst = document.createElement("h3");
+  var qst = document.createElement("h2");
   qst.innerText = currentQuestion.q;
   questionContainer.append(qst);
 
@@ -98,7 +109,7 @@ function renderQuestions() {
 function setTime() {
   var timerInterval = setInterval(function () {
     secondsLeft--;
-
+    score = secondsLeft;
     timerElement.textContent = "Time: " + secondsLeft;
 
     if (secondsLeft === 0) {
@@ -111,18 +122,52 @@ function endQuiz() {
 
   var initials = document.getElementById("initialSubmit");
   var endMessage = document.createElement("h2");
-  endMessage.innerHTML = "ALL DONE";
+  endMessage.innerHTML = "ALL DONE!";
   initials.append(endMessage);
+
+  var scoreMessage = document.createElement("h2");
+  scoreMessage.innerHTML = "Your final score is : " + score;
+  initials.append(scoreMessage);
+
   var elemForm = document.createElement("form");
   var initialText = document.createElement("textarea");
   elemForm.appendChild(initialText);
-  initials.append(elemForm);
   var submitButton = document.createElement("input");
   submitButton.setAttribute("type", "submit");
-  initials.append(submitButton);
+  elemForm.append(submitButton);
+  initials.append(elemForm);
 
-  submitButton.addEventListener("submit", function (event) {
+  elemForm.addEventListener("submit", function (event) {
     event.preventDefault();
     console.log(initialText.value);
+    initialSubmitContainer.innerHTML = "";
+    displayHighScores.classList.remove("hide");
+    // var highScoreMsg = document.createElement("h2");
+    // highScoreMsg.innerHTML = "High Scores:";
+    // displayHighScores.append(highScoreMsg);
+    var goBackbtn = document.createElement("button");
+    goBackbtn.setAttribute("value", "GoBack");
+
+    goBackbtn.textContent = "GoBack";
+    displayHighScores.append(goBackbtn);
+
+    var playScore = {
+      initials: initialText.value,
+      score: score,
+    };
+    // var setHighScores = localStorage.getItem(JSON.parse("setHighScores")) || [];
+    setHighScores.push(playScore);
+    // setHighScores.push(score);
+    localStorage.setItem("setHighScores", JSON.stringify(setHighScores));
+    // var getHighScores = localStorage.getItem(JSON.parse("setHighScores"));
+    // console.log(getHighScores);
   });
 }
+for (let i = 0; i < setHighScores.length; i++) {
+  var h2El = document.createElement("h2");
+  h2El.innerHTML = `${setHighScores[i].initials} : ${setHighScores[i].score}`;
+  displayHighScores.appendChild(h2El);
+  console.log(setHighScores[i]);
+}
+
+//setHighScores[i].initials + ": " + setHighScores[i].score
